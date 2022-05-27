14c52736318a9e34eb43468f8135b34070b21e98
macs1756 <kindrativmaksum312@gmail.com>
2022-05-27 11:08:20 +0200

    main block v1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 
+const burgerBtn = document.querySelector("#burgerBtn");
+const burgerContent = document.querySelector("#burgerContent");
+
+burgerBtn.addEventListener("click", function(){
+	burgerBtn.classList.toggle("burger__button_active");
+	burgerContent.classList.toggle("trans");
+});
+
+
